42b77d20c614d365265fbcadeeba412752625e6e
YastrebOlga <80920084+YastrebOlga@users.noreply.github.com>
2021-03-31 16:32:26 +0200

    Add files via upload

---
+++ HM1.js
@@ -0,0 +1,6 @@
+ 'use strict';
+ let nameVasiliy = "Василий";
+ let admin = nameVasiliy;
+ alert(admin);
+
+ // 3. Чему будет равно JS - выражение 1000 + "108"? Ответ:  1000- цифра,а "108"-строказначи 1000108
